1e2f9eb09247edd154a386197aae2869d77c7fe8
luizmoitinho <luiz.moitinho@olxbr.com>
2023-03-04 17:55:45 -0300

    feat: add md5 hash method

--- crypto_utils/crypto_utils.go
+++ crypto_utils/crypto_utils.go
@@ -1,6 +1,7 @@
 package crypto_utils
 
 import (
+	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
 )
@@ -9,6 +10,13 @@ func GetSha256(input string) string {
 	hash := sha256.New()
 	defer hash.Reset()
 
+	hash.Write([]byte(input))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func GetMd5(input string) string {
+	hash := md5.New()
+	defer hash.Reset()
 	hash.Write([]byte(input))
 	return hex.EncodeToString(hash.Sum(nil))
 }
